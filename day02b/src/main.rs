0f3ec7aede077ce49ad01fac0ab09f992d3ce8a7
Andy Belle-Isle <abelleisle@protonmail.com>
2022-12-01 23:50:53 -0600

    Slightly better day02b

--- day02b/src/main.rs
+++ day02b/src/main.rs
@@ -9,43 +9,18 @@ fn main()
     = include_str!("../input.txt")
         .lines()
         .map(|l| {
-            let mut char = "_";
-            let mut score = 0;
-            l.split(" ")
-            .for_each(|x| {
-                match x {
-                    "A"|"B"|"C" => char = x,
-                    "X" => { // Lose
-                        score += 0;
-                        match char {
-                            "A" => score += 3, // Rock
-                            "B" => score += 1, // Paper
-                            "C" => score += 2, // Scissors
-                            _ => score += 0
-                        }
-                    },
-                    "Y" => { // Draw
-                        score += 3;
-                        match char {
-                            "A" => score += 1, // Rock
-                            "B" => score += 2, // Paper
-                            "C" => score += 3, // Scissors
-                            _ => score += 0
-                        }
-                    },
-                    "Z" => { // Win
-                        score += 6;
-                        match char {
-                            "A" => score += 2, // Rock
-                            "B" => score += 3, // Paper
-                            "C" => score += 1, // Scissors
-                            _ => score += 0
-                        }
-                    },
-                    _ => {}
-                }
-            });
-            return score
+            match l {
+                "A X" => 0 + 3,
+                "B X" => 0 + 1,
+                "C X" => 0 + 2,
+                "A Y" => 3 + 1,
+                "B Y" => 3 + 2,
+                "C Y" => 3 + 3,
+                "A Z" => 6 + 2,
+                "B Z" => 6 + 3,
+                "C Z" => 6 + 1,
+                _ => 0
+            }
         }).sum();
 
     println!("{}", lines);
