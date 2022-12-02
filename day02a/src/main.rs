fcea50ab6d08bdb3402ebaec1fc1363ca5d77e51
Andy Belle-Isle <abelleisle@protonmail.com>
2022-12-01 23:55:15 -0600

    Slighty better 02a

--- day02a/src/main.rs
+++ day02a/src/main.rs
@@ -4,43 +4,18 @@ fn main()
     = include_str!("../input.txt")
         .lines()
         .map(|l| {
-            let mut char = "_";
-            let mut score = 0;
-            l.split(" ")
-            .for_each(|x| {
-                match x {
-                    "A"|"B"|"C" => char = x,
-                    "X" => { // Rock
-                        score += 1;
-                        match char {
-                            "A" => score += 3,
-                            "B" => score += 0,
-                            "C" => score += 6,
-                            _ => score += 0
-                        }
-                    },
-                    "Y" => { // Paper
-                        score += 2;
-                        match char {
-                            "A" => score += 6, // Rock
-                            "B" => score += 3, // Paper
-                            "C" => score += 0, // Scissors
-                            _ => score += 0
-                        }
-                    },
-                    "Z" => { // Scissors
-                        score += 3;
-                        match char {
-                            "A" => score += 0, // Rock
-                            "B" => score += 6,
-                            "C" => score += 3,
-                            _ => score += 0
-                        }
-                    },
-                    _ => {}
-                }
-            });
-            return score
+            match l {
+                "A X" => 1 + 3,
+                "B X" => 1 + 0,
+                "C X" => 1 + 6,
+                "A Y" => 2 + 6,
+                "B Y" => 2 + 3,
+                "C Y" => 2 + 0,
+                "A Z" => 3 + 0,
+                "B Z" => 3 + 6,
+                "C Z" => 3 + 3,
+                _ => 0
+            }
         }).sum();
 
     println!("{}", lines);
