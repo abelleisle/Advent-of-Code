9e254b509451885fcba3bb82ac041bf516b91f30
Andy Belle-Isle <drumsetmonkey@gmail.com>
2022-12-14 04:00:50 -0500

    Increased efficiency of solver

--- 2022/rust/src/days/day14/mod.rs
+++ 2022/rust/src/days/day14/mod.rs
@@ -40,7 +40,7 @@ pub fn parse(input: &str) -> Input {
         });
 
     let floor = cave.iter().map(|k| k.0.1).max().unwrap() + 2;
-    for x in -10000..10000 {
+    for x in -2000..2000 {
         cave.insert((x,floor), Env::Floor);
     }
 
@@ -74,12 +74,12 @@ pub fn draw(cave: &mut Input, sand: (isize, isize)) {
     for y in ys..=ye {
         for x in xs..=xe {
             if sand.eq(&(x,y)) {
-                draw_at((x-tl,y-tr), 'o', 43);
+                draw_at((x-tl,y-tr), ' ', 43);
             } else {
                 if let Some(c) = cave.get(&(x,y)) {
                     match c {
                         Env::Stone => draw_at((x-tl,y-tr), '#', 47),
-                        Env::Sand  => draw_at((x-tl,y-tr), '0', 43),
+                        Env::Sand  => draw_at((x-tl,y-tr), ' ', 43),
                         _ => {}
                     }
                 } else {
@@ -91,16 +91,17 @@ pub fn draw(cave: &mut Input, sand: (isize, isize)) {
     print!("{crt}");
 }
 
-pub fn sand_fall(cave: &mut Input, fall: bool, animate: bool, fps: u64) -> bool {
+pub fn sand_fall(cave: &mut Input, floor: isize, fall: bool, animate: bool, fps: u64) -> bool {
     let mut sand = (500, 0);
-    let floor = cave.iter().map(|k| k.0.1).max().unwrap();
     let sleep_cycle = 1000/fps.max(1);
 
     loop {
+        /* Do we want to animate this? */
         if animate {
             draw(cave, sand);
             std::thread::sleep(std::time::Duration::from_millis(sleep_cycle));
         }
+
         /* If we hit the floor, just stop and return */
         if sand.1 + 1 >= floor && !fall{
             return false;
@@ -142,8 +143,9 @@ pub fn sand_fall(cave: &mut Input, fall: bool, animate: bool, fps: u64) -> bool 
 }
 
 pub fn part1(mut input: Input) -> usize {
+    let floor = input.iter().map(|k| k.0.1).max().unwrap();
     loop {
-        if !sand_fall(&mut input, false, true, 60) {
+        if !sand_fall(&mut input, floor, false, true, 100) {
             break;
         }
     }
@@ -152,8 +154,9 @@ pub fn part1(mut input: Input) -> usize {
 }
 
 pub fn part2(mut input: Input) -> usize {
+    let floor = input.iter().map(|k| k.0.1).max().unwrap();
     loop {
-        if !sand_fall(&mut input, true, false, 0) {
+        if !sand_fall(&mut input, floor, true, false, 0) {
             break;
         }
     }
